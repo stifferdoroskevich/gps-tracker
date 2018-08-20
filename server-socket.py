f98884a52ad440fe7ea28dc2d1715558b9cd810e
Stiffer Doroskevich <stiffer17@gmail.com>
2018-08-19 23:27:23 -0400

    texto

--- server-socket.py
+++ server-socket.py
@@ -7,10 +7,12 @@ import sys
 from _thread import *
 
 #HOST = '172.16.1.194'   # Symbolic name meaning all available interfaces
-HOST = 'localhost'
+HOST = ''
 PORT = 47778 # Arbitrary non-privileged port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Content-Type: text/plain")
+print("")
 print('Socket created')
 
 #Bind socket to local host and port
