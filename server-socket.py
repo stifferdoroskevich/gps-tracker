0f11d719cd5f9fd58ba7a2dc94d24da0e8f750a2
Stiffer Doroskevich <stiffer17@gmail.com>
2018-08-19 20:53:10 -0400

    ajustado print

--- server-socket.py
+++ server-socket.py
@@ -11,20 +11,20 @@ HOST = "gps-tracker.herokuapp.com"
 PORT = 8888 # Arbitrary non-privileged port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
+print('Socket created')
 
 #Bind socket to local host and port
 try:
     s.bind((HOST, PORT))
 except socket.error as msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
 
-print 'Socket bind complete'
+print('Socket bind complete')
 
 #Start listening on socket
 s.listen(10)
-print 'Socket now listening'
+print('Socket now listening')
 
 
 def log(msg):
@@ -44,7 +44,7 @@ def clientthread(conn):
         if not data:
             break
 
-        print str(data)
+        print(str(data))
         log(data)
 
         if data.find("##") > -1:
@@ -60,7 +60,7 @@ def clientthread(conn):
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
     #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
     start_new_thread(clientthread ,(conn,))
